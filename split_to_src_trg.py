563466191d0cf4e8d88af5b651b2c1c387830123
Simon Gabay <gabay.simon@gmail.com>
2024-06-08 17:25:55 +0200

    update create data script

--- split_to_src_trg.py
+++ split_to_src_trg.py
@@ -5,7 +5,7 @@ import argparse
 
 #For colab or other notebook
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--colab", required=False, default='', help="Use this if you use colab")
+parser.add_argument("-c", "--colab", action='store_true', required=False, default='', help="Use this if you use colab")
 args = parser.parse_args()
 
 if args.colab:
